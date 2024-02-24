f5de3e73a84ddbdf4d3212afcd446e6ec9760518
alexandre <zatti.alexandre61@gmail.com>
2024-02-24 16:49:37 -0300

    Header pattern mobile and desktop tailwind setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,13 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        "font-color": "#272727"
       },
+      backgroundImage: {
+        "mobile": "url(/pattern-bg-mobile.png)",
+        "desktop": "url(/pattern-bg-desktop.png)"
+      }
     },
   },
   plugins: [],
